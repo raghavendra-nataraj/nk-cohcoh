40d8f7c98b9f52cb3519c48aef5569ed8db34b11
ragha <ragha@ragha.com>
2016-10-15 02:35:22 -0400

    iterative model

--- nkcohcoh.py
+++ nkcohcoh.py
@@ -3,6 +3,12 @@ import time
 import calendar
 cache = {}
 waitfor = 0
+
+class State():
+    def __init__(self,board,depth):
+        self.board = board
+        self.depth = depth
+
 def printBoard(board):
     for i in range(0,n):
         print " ".join(board[i*n:i*n+n])
@@ -21,14 +27,14 @@ def add_marble(board,index,chance):
     tmpBoard[index] = chance
     return tmpBoard
     
-def successors(board):
+def successors(st):
     chance = whoChance(board)
-    return [add_marble(board,idx,chance) for idx,i in enumerate(board) if i=='.']
+    return [State(add_marble(st.board,idx,chance),st.depth+1) for idx,i in enumerate(st.board) if i=='.']
 
 def isTerminal(board,chance):
     num = 0
     num = len([i for i in board if i!='.'])
-    if num == n**2-1: return 1
+    if num == n**2: return 1
     for i in range (0,n):
         for j in range(0,n):
             h = 0;v = 0;d1 = 0;d2 = 0
@@ -55,13 +61,13 @@ def checkStat(hl):
     result = False
     hl = sorted(set(hl))
     if hl == ['.','w']:
-        w+=1;n+=2
+        w+=2;n+=1
     if hl == ['w']:
-       n+=1;w+=4;result = True 
+       n+=1;w+=3;result = True 
     if hl == ['.','b']:
-        l+=1;n+=2
+        l+=2;n+=1
     if hl == ['b']:
-        n+=1;l+=4;result = True
+        n+=1;l+=3;result = True
     if hl == ['.']:
         n+=1;l+=1;w+=1
     if hl == ['b','w'] or hl == ['.','b','w']:
@@ -103,48 +109,51 @@ def evalFunc(board,chance):
     #printBoard(board)
     #print chance
     #print w,l
-    if  len([i for i in board if i!='.']) == n**2-1:
+    if  len([i for i in board if i!='.']) == n**2:
         result = True
     if chance =='w':
         return [num-w-l,result]
     else:
         return [num-l-w,result]
 
-def min_value(board,alpha,beta):
+def min_value(st,alpha,beta):
     global waitfor
-    chance = whoChance(board)
-    name = "".join(board)
+    chance = whoChance(st.board)
+    name = "".join(st.board)
     if name not in cache:
-        status = cache[name] = evalFunc(board,chance)
+        status = cache[name] = evalFunc(st.board,chance)
     status = cache[name]
     #if isTerminal(board,chance):
     if status[1]:
         return status[0]
-    val = min(beta,[max_value(state,alpha,beta) for state in successors(board) if  calendar.timegm(time.gmtime())<=waitfor])
+    vals = [max_value(state,alpha,beta) for state in successors(st) if st.depth < depth]
+    vals.append(beta)
+    val = min(vals)
     beta = min(beta,val)
     if alpha>=beta:return beta
     return val
 
-def max_value(board,alpha,beta):
+def max_value(st,alpha,beta):
     global waitfor
-    chance = whoChance(board)
-    name = "".join(board)
+    chance = whoChance(st.board)
+    name = "".join(st.board)
     if name not in cache:
-        status = cache[name] = evalFunc(board,chance)
+        status = cache[name] = evalFunc(st.board,chance)
     status = cache[name]
-    #if isTerminal(board,chance):
     if status[1]:
         return status[0]
-    val = max(alpha,[min_value(state,alpha,beta) for state in successors(board) if calendar.timegm(time.gmtime())<=waitfor])
+    vals = [min_value(state,alpha,beta) for state in successors(st) if st.depth < depth]
+    vals.append(alpha)
+    val = max(vals)
     alpha = max(alpha,val)
     if alpha>=beta:return alpha
     return val
 
-def solve(board):
+def solve(st):
     evalVal  = -sys.maxint - 1
     bb = []
-    chance = whoChance(board)
-    for state in successors(board):
+    chance = whoChance(st.board)
+    for state in successors(st):
         tempVal = max_value(state,-sys.maxint,sys.maxint)
         #print tempVal
         #printBoard(state)
@@ -152,15 +161,13 @@ def solve(board):
             evalVal = tempVal
             bb = state
     return bb
-
+depth = 2
 print sys.argv
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 board = list(sys.argv[3])
 t = int(sys.argv[4])
 waitfor =  calendar.timegm(time.gmtime())+t-1
-#printBoard(board)
-#print "\n"
 if whoChance(board)=="w":
     chance = "b"
 else:
@@ -168,8 +175,7 @@ else:
 if isTerminal(board,chance):
     print "Hey you lost"
 else:
-    printBoard(solve(board))
-#print evalFunc(board,'w')
-#print isTerminal(board,'w')
-#for succ in successors(board):
-   # printBoard(succ)
+    while waitfor>calendar.timegm(time.gmtime()):
+        depth+=1
+        goal = solve(State(board,0)).board
+printBoard(goal)
