1749e07c47ae5385c7c46f89bb9ce05590541907
ragha <ragha@ragha.com>
2016-10-16 21:32:57 -0400

    Report

--- nkcohcoh.py
+++ nkcohcoh.py
@@ -1,3 +1,29 @@
+'''
+(1) Abstraction for problem 1:
+
+a. Valid State, S = A nxn board with any possible arrangement of 'w' and 'b' such that number of 'w' and 'b' is the same or the number of 'w' is one more than 'b'. Also the number of continuous 'w' or 'b' in any row, column or diagonal cannot be greater than k.
+
+b. Initial State, S0 = A nxn board with a give combination of 'w' and 'b' such that number of 'w' and 'b' are same or the number of 'w' is one more than 'b'. Also the number of continuous 'w' or 'b'  in any row, column or diagonal is not greater than k. 
+
+c. Cost : The cost is constant since placing each marble has the same cost.
+
+d. Successors, Succ(S) = {S' | S' is a list of all possible ways the next 'w' (or 'b', based on input) can be placed in empty slots('.') on the given board}
+
+e. Goal State, G = {S | S has continuous k number of w's (or b's, whichever represents the opponent) in any row, column or diagonal of the board}
+number of 'w' or 'b' in state is not greater than k in any row, column or diagonal
+
+f. Heuristics : Taking different combinations of k marbles after adding the new marble and calculating the desirability of the state and deciding the most favorable state is the heuristics. For eg. [w,w,w] for a k value of 3 is very bad for while marble. So we give high negative weightage to the evaluation function. Similarly for the same value of k [.,.,.] is a favorable situation and also it does not prevent the opponent from losing. So it has a positive weightage. So we do this for all possible combinations and of states and add the wining and loosing chances and decide the most favorable state. 
+
+ 
+(2) Description of how the program works:
+Initially we take the current state and check if it is already a terminal state. If not, we generate successors. For each Successor, we call the the min_value for each state. The min_value will generate successors again and call max_value for each of the successors. This recursion will continue as long as the state is terminal state or the iteration depth has reached its limit. At each iteration we print the max of all min nodes for that depth. Then we increase the depth and try solving the problem again. This is repeated till the terminal is reached. The max value takes the max out of all the min_value functions. Similarly, the min_Value takes a minimum of all the max_value. We also prune the tree when the the min_value returns a value lesser than the parent max node. Similarly, we prune it when the max_value returns a value greater than the parent min node. I have not followed any logic is assigning weigtage to combination while calculating the evaluation function rather I have assigned the weights intuitively.
+
+(3) Problems faced and design decisions made:
+At certain times, the states had the same evaluation value but states in that ended in lesser depth than the others. So, it was not returning the most favorable state despite giving faster results. So, I introduced the depth factor in the evaluation function and that returned me the most favorable state. 
+
+'''
+
+
 import sys
 import time
 import calendar
